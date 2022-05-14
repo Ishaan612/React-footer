5d02d5eb9640702bcebef5c3f5187ec708640cb1
Ishaan <prashastaishan@gmail.com>
2022-05-14 15:04:04 +0530

    Updates

---
+++ precache-manifest.3368f7be9116ce3c685266ff9323c228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e95248f568dcf73c4f92ff2dea7deb",
+    "url": "/React-footer/index.html"
+  },
+  {
+    "revision": "344e1bbac9c60b4e932c",
+    "url": "/React-footer/static/js/2.8d098386.chunk.js"
+  },
+  {
+    "revision": "82e8035e49740f791fa94175bc04ac4e",
+    "url": "/React-footer/static/js/2.8d098386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c058bc2a668c2947a1af",
+    "url": "/React-footer/static/js/main.8d1e7479.chunk.js"
+  },
+  {
+    "revision": "9025b7b683d9d9853956",
+    "url": "/React-footer/static/js/runtime-main.2c798b27.js"
+  }
+]);
